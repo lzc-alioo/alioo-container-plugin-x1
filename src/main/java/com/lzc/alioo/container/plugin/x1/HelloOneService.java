bedf44c5018ee589ced32b3dff775c0b47f7f3cf
悟心 <zhichong.lzc@alibaba-inc.com>
2024-12-07 10:46:16 +0800

    support lib dir

--- src/main/java/com/lzc/alioo/container/plugin/x1/HelloOneService.java
+++ src/main/java/com/lzc/alioo/container/plugin/x1/HelloOneService.java
@@ -1,5 +1,6 @@
 package com.lzc.alioo.container.plugin.x1;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 
 public class HelloOneService {
@@ -16,11 +17,18 @@ public class HelloOneService {
         return model;
     }
 
+    /**
+     * 返回值类型是 jdk自带对象
+     *
+     * @param name
+     * @return
+     */
     public Object queryObject(String name) {
         HelloOneModel model = new HelloOneModel();
         model.setName(name);
         model.setAge(20);
         return model;
     }
+
 }
 
